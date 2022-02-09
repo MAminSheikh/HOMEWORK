7d1f8d41e2a6e6accf426dcbb256f1f20ef507b6
Amin_Shaikh <98902868+MAminSheikh@users.noreply.github.com>
2022-02-08 21:55:47 -0800

    Add files via upload

---
+++ new.dart
@@ -0,0 +1,41 @@
+void main () {
+    print ("Task 4");
+    int a1=145;
+    int a2=23;
+    int sum = a1 + a2 ;
+    
+    print (sum);
+    print ("Sum of ${a1} and ${a2} is : ${sum}");
+
+   // multiple 
+    int a3=42;
+    int a4=68;
+    int multi = a3 * a4;
+
+     print ("Multi");
+     print ("Multi of ${a1} and ${a2} is : ${a1*a2}");
+
+    //Divide 
+     int a5=123;
+     int a6=131;
+     double Divide = a5 / a6 ;
+
+     print (Divide);
+     print ("divide of ${a5} and ${a6} is : ${a5/a6}");
+
+     
+
+
+
+
+
+      
+
+
+
+
+
+     
+
+
+}
