61372e958bf77f888d416e675784dd5e9402c789
Amin_Shaikh <98902868+MAminSheikh@users.noreply.github.com>
2022-02-15 10:58:11 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -28,21 +28,65 @@ void main(){
     print("enter the tickets ${a1} ");
      print("number of tickets ${a2}");
      print ("total ticket price is ${a1 *a2}"); 
-   
-
-  
-  
-  
-
-   
-
-
-  
-   
-
-
-
-  
-  
-
+     print("\n");
+     print ("five task");
+     stdout.write("Enter the value of celsius:");
+     var cel = int.parse(stdin.readLineSync()!);
+     var fahren = (cel * 9 / 5)+ 32;
+     print ("celsius is convert to fahrenheit :${fahren}");
+     print ("\n");
+     print ("six task");
+     int first = 200,  second = 100; 
+     stdout.write("the price of first item :${first} \nthe price of second :${second}");
+     stdout.write("\nnumber of first item :");
+     int f1 = int.parse(stdin.readLineSync()!);
+     stdout.write("\nnumber of first item :");
+     int s1 = int.parse(stdin.readLineSync()!);
+     print("total price of :${(first * f1) + (second * s1)}");
+     print("seven task");
+     final int totalmark = 400;
+     stdout.write("the total number is ${totalmark} \nEnter the obtain number:");
+     var obn = int.parse(stdin.readLineSync()!);
+     print("the percentage is ${(obn / totalmark) * 100}");
+     print("\n");
+     print("Eight task");
+     double dou = 175.5;
+     var riyal = 46.8;
+     print("rate of dollar is :${dou}\nrate of riyal is ${riyal}");
+     stdout.write("enter number dollar: ");
+     int doll = int.parse(stdin.readLineSync()!);
+     stdout.write("enter number riyal: ");
+     int Rit = int.parse(stdin.readLineSync()!);
+     print("total covert in pakitani rupees :${(doll * dou) + (Rit * riyal)}");
+     print ("\n");
+     print ("last task");
+     stdout.write("Rate of hours :");
+     int hp = int.parse(stdin.readLineSync()!);
+     stdout.write("Enter number of hours: ");
+     int hours = int.parse(stdin.readLineSync()!);
+     print("total number of price is :${hp* hours}");
 }
+
+
+
+
+
+
+
+   
+
+  
+  
+  
+
+   
+
+
+  
+   
+
+
+
+  
+  
+
