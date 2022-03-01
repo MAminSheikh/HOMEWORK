b6dd86e230eedf31700fd25584745bdecca656e5
⚡️ Amin_Shaikh⚡️ <98902868+MAminSheikh@users.noreply.github.com>
2022-03-01 06:39:13 -0800

    Add files via upload

---
+++ main5.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main() {
+  List Student = [
+    {
+      "NAME": "ALI khan",
+      "FNAME": "Ansu khan",
+      "subject": ["English", "Math", "Android"],
+      "Subject MARK": {"English": 100, "Math": 100, "Android": 100},
+      "Obtained": {"English": 91, "Math": 79, "Android": 98},
+      "LOCation": {
+        "HOME Address": "Hosue # 80 Block b unit # 4",
+        "TAHSIL": "Latifabad",
+        "CITY": "HYDERBAD",
+        "PROVINSE": "SINDH",
+        "POSTAL CODE": '7100'
+      },
+      "placeofbirth": "Sindh"
+    },
+    {"NAME": "Faisal", "fNAME": "Rashid"},
+    {"NAME": "UBaid", "FNAME": "Ansu"}
+  ];
+}
